9039fe3d082b4032dd7852286e95e6e28701aaeb
salimisaeed <saeedsalimi.ir@gmail.com>
2021-04-29 13:34:17 +0430

    commit-3

--- catalog/models.py
+++ catalog/models.py
@@ -42,6 +42,14 @@ class Book(models.Model):
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+
+    display_genre.short_description = 'Genre'
 ###########################################################################################
 class BookInstance(models.Model):
     """
